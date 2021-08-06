28476c7b3d6544639b711ae6083ef389548e7214
unknown <adam.stgr@gmail.com>
2021-08-05 21:58:20 -0400

    finished all regular parts of the studio

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ window.addEventListener('load', function() {
     const takenOffBtn = document.getElementById('takeoff');
     const landingBtn = document.getElementById("landing");
     const abortBtn = document.getElementById("missionAbort");
+    const up = document.getElementById("up");
+    const imgObj = document.getElementById("rocket");
+    imgObj.style.position = "absolute";
+    imgObj.style.left = "160px";
+    imgObj.style.bottom = "193.12px";
 
     takenOffBtn.addEventListener('click', function() {
         const result = window.confirm(
@@ -13,8 +18,7 @@ window.addEventListener('load', function() {
             shuttleBackground.style.backgroundColor = "blue" 
             spaceShuttleHeight.innerHTML = 10000
         }
-    }
-    )
+    })
 
     landingBtn.addEventListener('click', function() {
         const thing = window.alert("The shuttle is landing. Landing gear engaged.");
@@ -25,11 +29,32 @@ window.addEventListener('load', function() {
 
     abortBtn.addEventListener('click', function() {
        const thing2 = window.confirm("Confirm that you want to abort the mission.");
-       if (result) {
+       if (thing2) {
            flightStatus.innerHTML = "Mission aborted."
            shuttleBackground.style.backgroundColor = "green";
            spaceShuttleHeight.innerHTML = 0;
        }
+    })
 
+    up.addEventListener('click', function() {
+        movement = parseInt(imgObj.style.bottom) + 10 + 'px';
+        imgObj.style.bottom = movement;
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000
+    })
+
+    down.addEventListener('click', function() {
+        movement = parseInt(imgObj.style.bottom) - 10 + 'px';
+        imgObj.style.bottom = movement;
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) - 10000
+    })
+
+    left.addEventListener('click', function() {
+        movement = parseInt(imgObj.style.left) - 10 + 'px';
+        imgObj.style.left = movement;
+    })
+
+    right.addEventListener('click', function() {
+        movement = parseInt(imgObj.style.left) + 10 + 'px';
+        imgObj.style.left = movement;
     })
 });
