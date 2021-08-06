232a19fc0674548aea8fc1f002be9798fae3032d
Emily Avener Haynes <avener@Emilys-MacBook-Air.local>
2021-08-05 20:53:17 -0400

    completed part 4

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function() {
     const flightStatus = document.getElementById('flightStatus');
     const takenOffBtn = document.getElementById('takeoff');
-    const landingBtn = document.getElementById("landing")
+    const landingBtn = document.getElementById("landing");
+    const abortBtn = document.getElementById("missionAbort");
+
     takenOffBtn.addEventListener('click', function() {
         const result = window.confirm(
             "Are you sure the shuttle is ready for takeoff?"
@@ -19,5 +21,15 @@ window.addEventListener('load', function() {
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
         spaceShuttleHeight.innerHTML = 0
+    })
+
+    abortBtn.addEventListener('click', function() {
+       const thing2 = window.confirm("Confirm that you want to abort the mission.");
+       if (result) {
+           flightStatus.innerHTML = "Mission aborted."
+           shuttleBackground.style.backgroundColor = "green";
+           spaceShuttleHeight.innerHTML = 0;
+       }
+
     })
 });
