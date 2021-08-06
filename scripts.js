bb7879385225c3f0dcccf1178de18e6e90fa16e3
unknown <adam.stgr@gmail.com>
2021-08-05 20:41:39 -0400

    part 3 of studio done

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 window.addEventListener('load', function() {
     const flightStatus = document.getElementById('flightStatus');
     const takenOffBtn = document.getElementById('takeoff');
+    const landingBtn = document.getElementById("landing")
     takenOffBtn.addEventListener('click', function() {
         const result = window.confirm(
             "Are you sure the shuttle is ready for takeoff?"
         );
-    
         if (result) {
             flightStatus.innerHTML = "<b> Shuttle in flight! </b>"
             shuttleBackground.style.backgroundColor = "blue" 
             spaceShuttleHeight.innerHTML = 10000
         }
     }
-
     )
+
+    landingBtn.addEventListener('click', function() {
+        const thing = window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = 0
+    })
 });
