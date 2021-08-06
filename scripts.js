b8046c99fb1b36018fafca0954fc8a6c537cf35f
Sruiz7316 <s.ruiz@live.iup.edu>
2021-08-05 20:20:57 -0400

    completed section 2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+    const flightStatus = document.getElementById('flightStatus');
+    const takenOffBtn = document.getElementById('takeoff');
+    takenOffBtn.addEventListener('click', function() {
+        const result = window.confirm(
+            "Are you sure the shuttle is ready for takeoff?"
+        );
+    
+        if (result) {
+            flightStatus.innerHTML = "<b> Shuttle in flight! </b>"
+            shuttleBackground.style.backgroundColor = "blue" 
+            spaceShuttleHeight.innerHTML = 10000
+        }
+    }
+
+    )
+});
